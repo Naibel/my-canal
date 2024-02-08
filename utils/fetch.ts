19750ad8f62c4a355b0054e194d21aec9471afd7
Naibel <dorian.belhaj@gmail.com>
2024-02-08 17:11:19 +0100

    feat: add types and const to fetch file

--- utils/fetch.ts
+++ utils/fetch.ts
@@ -1,16 +1,17 @@
+import { APIResponse } from "@_types/api";
 import axios from "axios";
 
+const API_URL = "https://api.themoviedb.org/3";
+
+//----FETCHING FUNCTIONS----
 export const fetch = async (endpoint: string) => {
   try {
-    const response = await axios.get(
-      "https://api.themoviedb.org/3" + endpoint,
-      {
-        headers: {
-          Accept: "application/json",
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_ACCESS_TOKEN}`,
-        },
-      }
-    );
+    const response = await axios.get(API_URL + endpoint, {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_ACCESS_TOKEN}`,
+      },
+    });
     const data = response.data;
     if (data) return data;
   } catch (err) {
@@ -18,10 +19,11 @@ export const fetch = async (endpoint: string) => {
   }
 };
 
-export const fetchTrendingMovies = async () => {
+export const fetchTrendingMovies = async (): Promise<APIResponse> => {
   return await fetch("/trending/movie/day?language=en-US");
 };
 
-export const fetchTrendingSeries = async () => {
+export const fetchTrendingSeries = async (): Promise<APIResponse> => {
   return await fetch("/trending/tv/day?language=en-US");
 };
+//
