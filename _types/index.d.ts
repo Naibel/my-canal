f112eee10673e51e2ea40b0c2f48d15b0b35cb2d
Naibel <dorian.belhaj@gmail.com>
2024-02-09 10:28:16 +0100

    feat: add new data to use on modal details

--- _types/index.d.ts
+++ _types/index.d.ts
@@ -1,5 +1,13 @@
+import {
+  EpisodeDetails,
+  Genre,
+  ProductionCompany,
+  SpokenLanguages,
+} from "./api";
+
 export type ModalDetails = {
   bgImage: string;
+  genres: Array<Genre>;
   homepage: string;
   id: number;
   nbOfVotes: number;
@@ -8,18 +16,29 @@ export type ModalDetails = {
   rating: number;
   title: string;
   yearOfRelease: number;
+  status: string;
+  tagline: string;
 };
 
 export type ModalMovieDetails = ModalDetails & {
+  boxOffice: string;
+  budget: string;
+  imdbUrl: string;
   mediaType: "movie";
+  productionCompanies: Array<ProductionCompany>;
+  releaseDate: string;
+  runtime: number;
+  spokenLanguages: Array<SpokenLanguages>;
 };
 
 export type ModalTVDetails = ModalDetails & {
+  createdBy: Array<Person>;
   firstAirDate: string;
   lastAirDate: string;
+  lastEpisodeToAir: EpisodeDetails;
   mediaType: "tv";
   networks: Array<Network>;
+  nextEpisodeToAir: EpisodeDetails;
   nbOfEpisodes: number;
   nbOfSeasons: number;
-  status: string;
 };
--- utils/format.ts
+++ utils/format.ts
@@ -5,15 +5,31 @@ import { APIMovieDetails, APISerieDetails } from "@_types/api";
 export const formatMovieData = (
   apiData: APIMovieDetails
 ): ModalMovieDetails => {
+  // Format the price above to USD using the locale, style, and currency.
+  let USDollar = new Intl.NumberFormat("fr-FR", {
+    style: "currency",
+    currency: "USD",
+  });
+
   return {
     bgImage: `https://image.tmdb.org/t/p/original${apiData.backdrop_path}`,
+    boxOffice: apiData.revenue ? USDollar.format(apiData.revenue) : "",
+    budget: apiData.budget ? USDollar.format(apiData.budget) : "",
+    genres: apiData.genres,
     homepage: apiData.homepage,
     id: apiData.id,
+    imdbUrl: `https://imdb.com/title/${apiData.imdb_id}`,
     mediaType: "movie",
     nbOfVotes: apiData.vote_count,
     overview: apiData.overview,
     originalTitle: apiData.original_title,
+    productionCompanies: apiData.production_companies,
     rating: apiData.vote_average,
+    releaseDate: new Date(apiData.release_date).toLocaleDateString("fr"),
+    runtime: apiData.runtime,
+    spokenLanguages: apiData.spoken_languages,
+    status: apiData.status === "Released" ? "Déjà sorti" : "Pas encore sorti",
+    tagline: apiData.tagline,
     title: apiData.title,
     yearOfRelease: new Date(apiData.release_date).getFullYear(),
   };
@@ -22,11 +38,15 @@ export const formatMovieData = (
 export const formatTVData = (apiData: APISerieDetails): ModalTVDetails => {
   return {
     bgImage: `https://image.tmdb.org/t/p/original${apiData.backdrop_path}`,
+    createdBy: apiData.created_by,
     firstAirDate: new Date(apiData.first_air_date).toLocaleDateString("fr"),
+    genres: apiData.genres,
     homepage: apiData.homepage,
     id: apiData.id,
     lastAirDate: new Date(apiData.last_air_date).toLocaleDateString("fr"),
+    lastEpisodeToAir: apiData.last_episode_to_air,
     mediaType: "tv",
+    nextEpisodeToAir: apiData.next_episode_to_air,
     nbOfEpisodes: apiData.number_of_episodes,
     nbOfSeasons: apiData.number_of_seasons,
     nbOfVotes: apiData.vote_count,
@@ -34,7 +54,8 @@ export const formatTVData = (apiData: APISerieDetails): ModalTVDetails => {
     overview: apiData.overview,
     originalTitle: apiData.original_name,
     rating: apiData.vote_average,
-    status: apiData.status === "Ended" ? "Terminée" : "En cours de diffusion",
+    status: apiData.in_production ? "En cours de diffusion" : "Terminée",
+    tagline: apiData.tagline,
     title: apiData.name,
     yearOfRelease: new Date(apiData.first_air_date).getFullYear(),
   };
