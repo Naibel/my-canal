8efd2f8c5ebba66c713d1c19951a24dfe36eb97f
Naibel <dorian.belhaj@gmail.com>
2024-02-10 15:17:53 +0100

    fix: add text inside items and generic typing inside carousel

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,25 +1,25 @@
 import Item from "@components/Item/Item";
-import {
-  APITrendingMovie,
-  APITrendingMovies,
-  APITrendingSerie,
-  APITrendingSeries,
-} from "@_types/api";
 import styles from "./Carousel.module.css";
 
-export interface CarouselProps {
-  data: APITrendingMovies | APITrendingSeries;
+export interface CarouselProps<T> {
+  data: Array<T>;
   onClick: (id: number) => void;
 }
 
-const Carousel = ({ data, onClick }: CarouselProps) => (
+const Carousel = <
+  T extends { id: number; poster_path: string; name?: string; title?: string }
+>({
+  data,
+  onClick,
+}: CarouselProps<T>) => (
   <div className={`${styles.carousel_wrapper} mt-4 overflow-x-auto`}>
     <div className={`${styles.carousel} flex gap-5`}>
-      {data?.map((item: APITrendingMovie | APITrendingSerie) => (
+      {data?.map((item: T) => (
         <Item
           key={item.id}
           poster={item.poster_path}
           onClick={() => onClick(item.id)}
+          title={item.name || item.title || ""}
         />
       ))}
     </div>
--- components/Item/Item.tsx
+++ components/Item/Item.tsx
@@ -1,12 +1,13 @@
+import { LogoMyCanal } from "@components/NavBar/LogoMyCanal";
 import styles from "./Item.module.css";
 
-const Item = ({
-  poster,
-  onClick,
-}: {
+type ItemProps = {
   poster?: string;
   onClick?: () => void;
-}) => (
+  title: string;
+};
+
+const Item = ({ poster, onClick, title }: ItemProps) => (
   <div
     style={{
       backgroundImage: poster
@@ -18,8 +19,18 @@ const Item = ({
     onClick={onClick}
     className={`${styles.icon} ${
       onClick ? "" : "animate-pulse"
-    }  w-1/3 bg-no-repeat bg-cover bg-center bg-neutral-600 p-3 rounded-md shadow-sm hover:shadow-md`}
-  ></div>
+    }  w-full flex text-center px-5 justify-center flex-col gap-5 items-center bg-no-repeat bg-cover bg-center bg-neutral-600 rounded-md shadow-sm hover:shadow-md hover:bg-neutral-700 active:bg-neutral-800`}
+  >
+    {!poster ? (
+      <>
+        <LogoMyCanal />
+        <span className="uppercase italic font-semibold mb-2">{title}</span>
+      </>
+    ) : (
+      <></>
+    )}
+    <div className="absolute bg-gradient-to-r from-indigo-500 "></div>
+  </div>
 );
 
 export default Item;
