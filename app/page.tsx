3652065fc6a81a1c61d3884150e190bcf8dc5538
Naibel <dorian.belhaj@gmail.com>
2024-02-10 15:10:47 +0100

    fix: remove refetch when windows on focus

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import HomePage from "@views/HomePage";
+
+import HomePage from "@views/HomePage/HomePage";
 
 // Create a client
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchIntervalInBackground: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function Home() {
   return (
