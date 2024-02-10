4208111b3b52a68d45d14ddf47baf44625f03153
Naibel <dorian.belhaj@gmail.com>
2024-02-10 15:13:15 +0100

    fix(format): add more value check and create separate format functions

--- utils/format.ts
+++ utils/format.ts
@@ -1,30 +1,42 @@
 import { ModalMovieDetails, ModalTVDetails } from "@_types";
-import { APIMovieDetails, APISerieDetails } from "@_types/api";
+import { APIMovieDetails, APITVSeriesDetails } from "@_types/api";
 
-//FORMAT FUNCTIONS
-export const formatMovieData = (
-  apiData: APIMovieDetails
-): ModalMovieDetails => {
+const formatDateToFrLocale = (time: string) => {
+  return time ? new Date(time).toLocaleDateString("fr") : "";
+};
+
+const formatToUSD = (sum: number) => {
   let USDollar = new Intl.NumberFormat("fr-FR", {
     style: "currency",
     currency: "USD",
   });
 
+  return sum ? USDollar.format(sum) : "";
+};
+
+//FORMAT FUNCTIONS
+export const formatMovieData = (
+  apiData: APIMovieDetails
+): ModalMovieDetails => {
   return {
-    bgImage: `https://image.tmdb.org/t/p/original${apiData.backdrop_path}`,
-    boxOffice: apiData.revenue ? USDollar.format(apiData.revenue) : "",
-    budget: apiData.budget ? USDollar.format(apiData.budget) : "",
+    bgImage: apiData.backdrop_path
+      ? `https://image.tmdb.org/t/p/original${apiData.backdrop_path}`
+      : undefined,
+    boxOffice: formatToUSD(apiData.revenue),
+    budget: formatToUSD(apiData.budget),
     genres: apiData.genres,
     homepage: apiData.homepage,
     id: apiData.id,
-    imdbUrl: `https://imdb.com/title/${apiData.imdb_id}`,
+    imdbUrl: apiData.backdrop_path
+      ? `https://imdb.com/title/${apiData.imdb_id}`
+      : undefined,
     mediaType: "movie",
     nbOfVotes: apiData.vote_count,
     overview: apiData.overview,
     originalTitle: apiData.original_title,
     productionCompanies: apiData.production_companies,
     rating: apiData.vote_average,
-    releaseDate: new Date(apiData.release_date).toLocaleDateString("fr"),
+    releaseDate: formatDateToFrLocale(apiData.release_date),
     runtime: apiData.runtime,
     spokenLanguages: apiData.spoken_languages,
     status: apiData.status === "Released" ? "Déjà sorti" : "Pas encore sorti",
@@ -34,20 +46,20 @@ export const formatMovieData = (
   };
 };
 
-export const formatTVData = (apiData: APISerieDetails): ModalTVDetails => {
-  const checkStatus = () => {
-    if (apiData.in_production && apiData.status === "In Production")
-      return "En cours de production";
-    else if (apiData.in_production && apiData.status === "Returning Series")
-      return "En cours de diffusion";
-    else if (!apiData.in_production && apiData.status === "Ended")
-      return "Diffusion terminée";
-    else return "Inconnu";
-  };
+const checkStatus = (inProduction: boolean, status: string) => {
+  if (inProduction && status === "In Production")
+    return "En cours de production";
+  else if (inProduction && status === "Returning Series")
+    return "En cours de diffusion";
+  else if (!inProduction && status === "Ended") return "Diffusion terminée";
+  else return "Inconnu";
+};
+
+export const formatTVData = (apiData: APITVSeriesDetails): ModalTVDetails => {
   return {
     bgImage: `https://image.tmdb.org/t/p/original${apiData.backdrop_path}`,
     createdBy: apiData.created_by,
-    firstAirDate: new Date(apiData.first_air_date).toLocaleDateString("fr"),
+    firstAirDate: formatDateToFrLocale(apiData.first_air_date),
     genres: apiData.genres,
     homepage: apiData.homepage,
     id: apiData.id,
@@ -64,7 +76,8 @@ export const formatTVData = (apiData: APISerieDetails): ModalTVDetails => {
     overview: apiData.overview,
     originalTitle: apiData.original_name,
     rating: apiData.vote_average,
-    status: checkStatus(),
+    spokenLanguages: apiData.spoken_languages,
+    status: checkStatus(apiData.in_production, apiData.status),
     tagline: apiData.tagline,
     title: apiData.name,
     yearOfRelease: new Date(apiData.first_air_date).getFullYear(),
