3e4d136ef2fecd0527a2b875850f0aa68b6e7b03
Naibel <dorian.belhaj@gmail.com>
2024-02-10 15:11:21 +0100

    feat: add params on fetch query and search endpoints

--- utils/fetch.ts
+++ utils/fetch.ts
@@ -1,16 +1,17 @@
-import { APIResponse } from "@_types/api";
+import { APIMovie, APIResponse, APITVSeries } from "@_types/api";
 import axios from "axios";
 
 const API_URL = "https://api.themoviedb.org/3";
 
 //----FETCHING FUNCTIONS----
-export const fetch = async (endpoint: string) => {
+export const fetch = async (endpoint: string, params?: any) => {
   try {
     const response = await axios.get(API_URL + endpoint, {
       headers: {
         Accept: "application/json",
         Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_ACCESS_TOKEN}`,
       },
+      params,
     });
     const data = response.data;
     if (data) return data;
@@ -19,11 +20,25 @@ export const fetch = async (endpoint: string) => {
   }
 };
 
-export const fetchTrendingMovies = async (): Promise<APIResponse> => {
+export const fetchTrendingMovies = async (): Promise<APIResponse<APIMovie>> => {
   return await fetch("/trending/movie/day?language=en-US");
 };
 
-export const fetchTrendingSeries = async (): Promise<APIResponse> => {
+export const fetchTrendingSeries = async (): Promise<
+  APIResponse<APITVSeries>
+> => {
   return await fetch("/trending/tv/day?language=en-US");
+};
+
+export const searchMovies = async (
+  query: string
+): Promise<APIResponse<APIMovie>> => {
+  return await fetch("/search/movie", { query });
+};
+
+export const searchTVSeries = async (
+  query: string
+): Promise<APIResponse<APITVSeries>> => {
+  return await fetch("/search/tv", { query });
 };
 //
