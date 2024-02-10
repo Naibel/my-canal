766bb860a59320c2509d5b2b615af7d914e8d0cc
Naibel <dorian.belhaj@gmail.com>
2024-02-10 14:52:15 +0100

    style: move fonts description to its own file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import futura from "./fonts";
 
-const futura = localFont({
-  src: [
-    {
-      path: "../public/fonts/futura/Futura-Book-font.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/futura/Futura-Book-Italic-font.ttf",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/futura/Futura-Heavy-font.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/futura/Futura-Heavy-Italic-font.ttf",
-      weight: "600",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/futura/Futura-Bold-Italic-font.ttf",
-      weight: "700",
-      style: "italic",
-    },
-  ],
-});
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "My Canal",
