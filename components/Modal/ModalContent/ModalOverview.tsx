cdc558db944991e2bd7ce0d1837573ce73c16d19
Naibel <dorian.belhaj@gmail.com>
2024-02-11 18:37:56 +0100

    feat(modal): add production companies to tv series

--- components/Modal/ModalContent/ModalOverview.tsx
+++ components/Modal/ModalContent/ModalOverview.tsx
@@ -30,56 +30,53 @@ const ModalOverview = ({
           <p className="italic">Pas de résumé pour le moment...</p>
         )}
       </div>
+      {modalDetails.mediaType === "tv" && modalDetails.createdBy.length > 0 && (
+        <div>
+          <ModalTitle>Une série créée par</ModalTitle>
+          <div className="grid grid-rows-1 grid-cols-4 md:grid-cols-6 gap-5">
+            {modalDetails.createdBy.map((person: Person) => (
+              <PersonCard
+                key={person.id}
+                bgProfile={person.profile_path}
+                name={person.name}
+                gender={person.gender}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+      {modalDetails.productionCompanies?.length > 0 && (
+        <div>
+          <ModalTitle>Produit par</ModalTitle>
+          <div className="grid grid-rows-1 grid-cols-3 md:grid-cols-5 gap-5">
+            {modalDetails.productionCompanies.map(
+              (company: ProductionCompany) => (
+                <ProductionCard
+                  key={company.id}
+                  logo={company.logo_path}
+                  name={company.name}
+                />
+              )
+            )}
+          </div>
+        </div>
+      )}
       {modalDetails.mediaType === "tv" && (
-        <>
-          {modalDetails.createdBy.length > 0 && (
+        <div className="grid grid-rows-1 grid-cols-1 md:grid-cols-2 gap-5">
+          {modalDetails.lastEpisodeToAir && (
             <div>
-              <ModalTitle>Une série créée par</ModalTitle>
-              <div className="grid grid-rows-1 grid-cols-4 md:grid-cols-6 gap-5">
-                {modalDetails.createdBy.map((person: Person) => (
-                  <PersonCard
-                    key={person.id}
-                    bgProfile={person.profile_path}
-                    name={person.name}
-                    gender={person.gender}
-                  />
-                ))}
-              </div>
+              <ModalTitle>Dernier épisode diffusé</ModalTitle>
+              <EpisodeCard episode={modalDetails.lastEpisodeToAir} />
             </div>
           )}
-          <div className="grid grid-rows-1 grid-cols-1 md:grid-cols-2 gap-5">
-            {modalDetails.lastEpisodeToAir && (
-              <div>
-                <ModalTitle>Dernier épisode diffusé</ModalTitle>
-                <EpisodeCard episode={modalDetails.lastEpisodeToAir} />
-              </div>
-            )}
-            {modalDetails.nextEpisodeToAir && (
-              <div>
-                <ModalTitle>Prochain épisode</ModalTitle>
-                <EpisodeCard episode={modalDetails.nextEpisodeToAir} />
-              </div>
-            )}
-          </div>
-        </>
-      )}
-      {modalDetails.mediaType === "movie" &&
-        modalDetails.productionCompanies.length > 0 && (
-          <div>
-            <ModalTitle>Produit par</ModalTitle>
-            <div className="grid grid-rows-1 grid-cols-3 md:grid-cols-5 gap-5">
-              {modalDetails.productionCompanies.map(
-                (company: ProductionCompany) => (
-                  <ProductionCard
-                    key={company.id}
-                    logo={company.logo_path}
-                    name={company.name}
-                  />
-                )
-              )}
+          {modalDetails.nextEpisodeToAir && (
+            <div>
+              <ModalTitle>Prochain épisode</ModalTitle>
+              <EpisodeCard episode={modalDetails.nextEpisodeToAir} />
             </div>
-          </div>
-        )}
+          )}
+        </div>
+      )}
     </div>
   );
 };
