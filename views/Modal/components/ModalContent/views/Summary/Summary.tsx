fb9230b23df56e7a2fb496c49a9195ec333ce10a
Naibel <dorian.belhaj@gmail.com>
2024-02-14 18:49:54 +0100

    feat: create new MoreInfo section for modal

--- views/Modal/components/ModalContent/views/Summary/Summary.tsx
+++ views/Modal/components/ModalContent/views/Summary/Summary.tsx
@@ -1,11 +1,9 @@
 import { ModalMovieDetails, ModalTVDetails } from "~/types";
-import { Person, ProductionCompany } from "~/types/api";
+
+import SummarySection from "../../components/ModalSection";
 
 import EpisodePanel from "./EpisodePanel";
 import Genres from "./Genres";
-import PersonPanel from "./PersonPanel";
-import ProductionPanel from "./ProductionPanel";
-import SummarySection from "./SummarySection";
 
 const Summary = ({
   modalDetails,
@@ -27,35 +25,6 @@ const Summary = ({
         <p className="italic">Pas de résumé pour le moment...</p>
       )}
     </div>
-    {modalDetails.mediaType === "tv" && modalDetails.createdBy.length > 0 && (
-      <SummarySection title="Une série créée par">
-        <div className="grid grid-rows-1 grid-cols-4 md:grid-cols-6 gap-5">
-          {modalDetails.createdBy.map((person: Person) => (
-            <PersonPanel
-              key={person.id}
-              bgProfile={person.profile_path}
-              name={person.name}
-              gender={person.gender}
-            />
-          ))}
-        </div>
-      </SummarySection>
-    )}
-    {modalDetails.productionCompanies?.length > 0 && (
-      <SummarySection title="Produit par">
-        <div className="grid grid-rows-1 grid-cols-3 md:grid-cols-5 gap-2 md:gap-5">
-          {modalDetails.productionCompanies.map(
-            (company: ProductionCompany) => (
-              <ProductionPanel
-                key={company.id}
-                logo={company.logo_path}
-                name={company.name}
-              />
-            )
-          )}
-        </div>
-      </SummarySection>
-    )}
     {modalDetails.mediaType === "tv" && (
       <div className="grid grid-rows-1 grid-cols-1 md:grid-cols-2 gap-5">
         {modalDetails.lastEpisodeToAir && (
