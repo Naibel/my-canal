5f551a9cf3b204edede8ca65b13af9db03dc740e
Naibel <dorian.belhaj@gmail.com>
2024-02-14 18:29:27 +0100

    fix: trending page now at root level

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+import withProviders from "~/providers/withProviders.hoc";
+import { Trending } from "~/views";
+
+export default withProviders(Trending);
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -6,25 +6,28 @@ import LogoMyCanal from "../LogoMyCanal/LogoMyCanal";
 import NavbarButton from "./NavbarButton";
 import NavbarSearchInput from "./NavbarSearchInput";
 
-export type PageType = "trending" | "discover";
+export type PageType = "" | "discover";
 
 const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="grid grid-rows-2 grid-cols-1 md:flex md:flex-row gap-5 items-center bg-black px-5 py-4 shadow-md">
-      <LogoMyCanal />
-      <div className="flex gap-5 justify-center">
-        <NavbarButton to={"trending"} isActive={pathname === "/trending/"}>
-          Tendances
-        </NavbarButton>
-        <NavbarButton to={"discover"} isActive={pathname === "/discover/"}>
-          Découvrir
-        </NavbarButton>
-      </div>
-      <div className="flex-1 hidden md:block" />
-      <NavbarSearchInput />
-    </nav>
+    <div className="absolute z-10 left-0 right-0 flex flex-col overflow-auto">
+      <nav className="  grid grid-rows-2 grid-cols-1 md:flex md:flex-row gap-5 items-center bg-black px-5 py-4 shadow-md">
+        <LogoMyCanal />
+        <div className="flex gap-5 justify-center">
+          <NavbarButton to={""} isActive={pathname === "/"}>
+            Tendances
+          </NavbarButton>
+          <NavbarButton to={"discover"} isActive={pathname === "/discover/"}>
+            Découvrir
+          </NavbarButton>
+        </div>
+        <div className="flex-1 hidden md:block" />
+        <NavbarSearchInput />
+      </nav>
+      <div id="search_results" />
+    </div>
   );
 };
 
