4e2ee2d699b53bc4a8630a54e274e95ef16a6534
Naibel <dorian.belhaj@gmail.com>
2024-02-15 22:09:52 +0100

    style: footer now smaller on mobile

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer className="bg-black px-5 py-3 uppercase font-semibold italic">
+  <footer className="bg-black text-xs md:text-base px-2 py-2 md:px-5 md:py-3 uppercase font-semibold italic">
     Made by Dorian Belhaj - 2024
   </footer>
 );
