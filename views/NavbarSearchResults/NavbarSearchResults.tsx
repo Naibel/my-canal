06b57912c7445758eb1567350d609e3af88e96ea
Naibel <dorian.belhaj@gmail.com>
2024-02-15 22:10:28 +0100

    style(NBSR): smaller gaps between cards on mobile

--- views/NavbarSearchResults/NavbarSearchResults.tsx
+++ views/NavbarSearchResults/NavbarSearchResults.tsx
@@ -17,11 +17,11 @@ const NavbarSearchResults = <
   loading,
 }: NavbarSearchResultsProps<T>) => (
   <div className="flex flex-1 min-h-screen h-max bg-black left-0 right-0 top-0 h-full bottom-0">
-    <div className="flex flex-1 flex-col p-5 gap-5">
+    <div className="flex flex-1 flex-col p-3 gap-3 md:p-5 md:gap-5">
       <Title size="large">Résultats de la recherche</Title>
       {loading && <SearchLoadingMessage />}
       {!loading && results && results?.length > 0 && (
-        <div className="grid h-fit grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5">
+        <div className="grid h-fit grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-2 md:gap-5">
           {results?.map((item: T) => (
             <Card
               key={"card_" + item.id}
