57d6750fdeca81b7c4873192a5687514bc8b78ac
Naibel <dorian.belhaj@gmail.com>
2024-02-22 00:14:41 +0100

    feat(fetch): add fetching functions for cast and recommendations

--- utils/fetch.ts
+++ utils/fetch.ts
@@ -1,16 +1,17 @@
 import axios from "axios";
 
 import {
+  APIConfig,
   APIMovie,
+  APIMovieCredits,
   APIMovieDetails,
   APIResponse,
+  APITVCredits,
   APITVSeries,
   APITVSeriesDetails,
 } from "~/types/api";
-import { MediaType, ModalMovieDetails, ModalTVDetails } from "~/types/modal";
+import { MediaType } from "~/types/modal";
 import { DiscoverSearchForm } from "~/views/Discover/Discover";
-
-import { formatMovieData, formatTVData } from "./format";
 
 const API_URL = "https://api.themoviedb.org/3";
 export const IMAGE_PREFIX_URL = "https://image.tmdb.org/t/p/original";
@@ -58,6 +59,10 @@ export const discover = async (
 //
 
 //----GET FUNCTIONS----
+export const getAPIConfig = async (): Promise<APIConfig> => {
+  return await fetch("/configuration");
+};
+
 export const getDetails = async (
   pathname: string
 ): Promise<APIMovieDetails | APITVSeriesDetails> => {
@@ -66,35 +71,95 @@ export const getDetails = async (
   });
 };
 
-export const getTVDetails = async (
-  id: number,
-  onSuccess: (res: ModalTVDetails) => void,
-  onFailure: (error: any) => void
-) => {
-  fetch(`/tv/${id}`, { language: "fr-FR" })
-    .then((res: APITVSeriesDetails) => {
-      const details: ModalTVDetails = formatTVData(res);
-      onSuccess(details);
-    })
-    ?.catch((error: any) => {
-      console.error(error);
-      onFailure(error);
-    });
+export const getMovieDetails = async (id: string): Promise<APIMovieDetails> => {
+  return await fetch("/movie/" + id, {
+    language: "fr-FR",
+  });
 };
 
-export const getMovieDetails = async (
-  id: number,
-  onSuccess: (res: ModalMovieDetails) => void,
-  onFailure: (error: any) => void
-) => {
-  fetch(`/movie/${id}`, { language: "fr-FR" })
-    .then((res: APIMovieDetails) => {
-      const details: ModalMovieDetails = formatMovieData(res);
-      onSuccess(details);
-    })
-    ?.catch((error: any) => {
-      console.error(error);
-      onFailure(error);
-    });
+export const getMovieCreditsDetails = async (
+  id: string
+): Promise<APIMovieCredits> => {
+  return await fetch("/movie/" + id + "/credits", {
+    language: "fr-FR",
+  });
 };
+
+export const getMovieKeywords = async (
+  id: string
+): Promise<APIMovieDetails> => {
+  return await fetch("/movie/" + id + "/keywords", {
+    language: "fr-FR",
+  });
+};
+
+export const getMovieRecommendations = async (
+  id: string
+): Promise<APIResponse<APIMovie>> => {
+  return await fetch("/movie/" + id + "/recommendations", {
+    language: "fr-FR",
+  });
+};
+
+export const getTVDetails = async (id: string): Promise<APITVSeriesDetails> => {
+  return await fetch("/tv/" + id, {
+    language: "fr-FR",
+  });
+};
+
+export const getTVCreditsDetails = async (
+  id: string
+): Promise<APITVCredits> => {
+  return await fetch("/tv/" + id + "/aggregate_credits", {
+    language: "fr-FR",
+  });
+};
+
+export const getTVKeywords = async (
+  id: string
+): Promise<APITVSeriesDetails> => {
+  return await fetch("/tv/" + id + "/keywords", {
+    language: "fr-FR",
+  });
+};
+
+export const getTVRecommendations = async (
+  id: string
+): Promise<APIResponse<APITVSeries>> => {
+  return await fetch("/tv/" + id + "/recommendations", {
+    language: "fr-FR",
+  });
+};
+
+// export const getTVDetails = async (
+//   id: number,
+//   onSuccess: (res: ModalTVDetails) => void,
+//   onFailure: (error: any) => void
+// ) => {
+//   fetch(`/tv/${id}`, { language: "fr-FR" })
+//     .then((res: APITVSeriesDetails) => {
+//       const details: ModalTVDetails = formatTVData(res);
+//       onSuccess(details);
+//     })
+//     ?.catch((error: any) => {
+//       console.error(error);
+//       onFailure(error);
+//     });
+// };
+
+// export const getMovieDetails = async (
+//   id: number,
+//   onSuccess: (res: ModalMovieDetails) => void,
+//   onFailure: (error: any) => void
+// ) => {
+//   fetch(`/movie/${id}`, { language: "fr-FR" })
+//     .then((res: APIMovieDetails) => {
+//       const details: ModalMovieDetails = formatMovieData(res);
+//       onSuccess(details);
+//     })
+//     ?.catch((error: any) => {
+//       console.error(error);
+//       onFailure(error);
+//     });
+// };
 //
