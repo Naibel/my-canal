7c1bf881b90436375b0c7255463efa286ffb1cd8
Naibel <dorian.belhaj@gmail.com>
2024-02-22 00:39:30 +0100

    feat(NavBar): add link to details in cards

--- providers/NavbarSearchProvider.tsx
+++ providers/NavbarSearchProvider.tsx
@@ -2,15 +2,13 @@ import { ReactNode } from "react";
 import { createPortal } from "react-dom";
 import { useQuery } from "@tanstack/react-query";
 
-import { useDebounce, useModalFunctions, useNavbarSearchStore } from "~/hooks";
+import { useDebounce, useNavbarSearchStore } from "~/hooks";
 import { search } from "~/utils/fetch";
 
 import NavbarSearchResults from "../views/NavbarSearchResults/NavbarSearchResults";
 
 const NavbarSearchProvider = ({ children }: { children: ReactNode }) => {
   const { searchValue, searchMediaType } = useNavbarSearchStore();
-
-  const { displayMovieModal, displayTVModal } = useModalFunctions();
 
   const debouncedSearch = useDebounce(searchValue, 300);
   const searchResultsData = useQuery({
@@ -25,9 +23,7 @@ const NavbarSearchProvider = ({ children }: { children: ReactNode }) => {
           <NavbarSearchResults
             loading={searchResultsData.isPending}
             results={searchResultsData?.data?.results}
-            onClick={
-              searchMediaType === "tv" ? displayTVModal : displayMovieModal
-            }
+            searchMediaType={searchMediaType}
           />,
           document.getElementById("search_results")!
         )
--- views/NavbarSearchResults/NavbarSearchResults.tsx
+++ views/NavbarSearchResults/NavbarSearchResults.tsx
@@ -1,20 +1,23 @@
+import Link from "next/link";
+
 import { Card, Title } from "~/components";
+import { MediaType } from "~/types/modal";
 
 import { NoResultsFound } from "./components/NoResultsFound";
 import { SearchLoadingMessage } from "./components/SearchLoadingMessage";
 
 type NavbarSearchResultsProps<T> = {
   results: Array<T> | undefined;
-  onClick: (id: number) => void;
   loading: boolean;
+  searchMediaType: MediaType;
 };
 
 const NavbarSearchResults = <
   T extends { id: number; poster_path: string; name?: string; title?: string }
 >({
   results,
-  onClick,
   loading,
+  searchMediaType,
 }: NavbarSearchResultsProps<T>) => (
   <div className="flex flex-1 min-h-screen h-max bg-black left-0 right-0 top-0 h-full bottom-0">
     <div className="flex flex-1 flex-col p-3 gap-3 md:p-5 md:gap-5">
@@ -23,13 +26,15 @@ const NavbarSearchResults = <
       {!loading && results && results?.length > 0 && (
         <div className="grid h-fit grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-2 md:gap-5">
           {results?.map((item: T) => (
-            <Card
+            <Link
               key={"card_" + item.id}
-              id={item.id}
-              poster={item.poster_path}
-              onClick={() => onClick(item.id)}
-              title={item.name || item.title || ""}
-            />
+              href={`/${searchMediaType}/${item.id}`}
+            >
+              <Card
+                poster={item.poster_path}
+                title={item.name || item.title || ""}
+              />
+            </Link>
           ))}
         </div>
       )}
